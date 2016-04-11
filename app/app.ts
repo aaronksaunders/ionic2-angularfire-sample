6fc20479358a782bb88b26bb2057ace4cb5925fc
Aaron Saunders <aaron@clearlyinnovative.com>
2016-04-10 23:27:45 -0400

    added the ability to login

--- app/app.ts
+++ app/app.ts
@@ -4,13 +4,23 @@ import {StatusBar} from 'ionic-native';
 import {HomePage} from './pages/home/home';
 
 
-import {FIREBASE_PROVIDERS, defaultFirebase, AngularFire} from 'angularfire2';
+import {
+    FIREBASE_PROVIDERS, defaultFirebase,
+    AngularFire, firebaseAuthConfig, AuthProviders,
+    AuthMethods
+} from 'angularfire2';
 
 @App({
     template: '<ion-nav [root]="rootPage"></ion-nav>',
     providers: [
         FIREBASE_PROVIDERS,
-        defaultFirebase('https://clearlyinnovative-firebasestarterapp.firebaseio.com/')
+        defaultFirebase('https://clearlyinnovative-firebasestarterapp.firebaseio.com/'),
+        firebaseAuthConfig({
+            provider: AuthProviders.Password,
+            method: AuthMethods.Password,
+            remember: 'default',
+            scope: ['email']
+        })
     ],
     config: {} // http://ionicframework.com/docs/v2/api/config/Config/
 })
--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -3,15 +3,26 @@ import {Component} from 'angular2/core';
 import {AngularFire} from 'angularfire2';
 import {Observable} from 'rxjs/Observable';
 
+import {FirebaseAuth, AuthProviders, AuthMethods } from 'angularfire2';
+
 @Page({
     template: `
         <ion-navbar *navbar>
             <ion-title>
                 Home
             </ion-title>
+            <ion-buttons end>
+                <button (click)="loginClicked()">
+                <ion-icon name="contact"></ion-icon>
+                </button>
+            </ion-buttons>            
         </ion-navbar>
 
         <ion-content class="home">
+            <ion-item>
+                <div *ngIf="auth | async">You are logged in as {{authInfo.email}}</div>
+                <div *ngIf="!(auth | async)">Please log in</div>
+            </ion-item>
             <ion-card  *ngFor="#item of bookItems | async">
                 <ion-card-header>
                     {{item.volumeInfo.title}}
@@ -24,7 +35,42 @@ import {Observable} from 'rxjs/Observable';
 })
 export class HomePage {
     bookItems: Observable<any[]>;
-    constructor(af: AngularFire) {
+    authInfo: any
+
+    constructor(public af: AngularFire, public auth: FirebaseAuth) {
         this.bookItems = af.list('/bookItems');
+
+        auth.subscribe((data) => {
+            console.log("in auth subscribe", data)
+            if (data) {
+                this.authInfo = data.password
+            } else {
+                this.authInfo = null
+            }
+        })
+    }
+
+    loginClicked() {
+
+        if (this.authInfo && this.authInfo.email) {
+            this.auth.logout();
+            return;
+        }
+
+        // This will perform popup auth with google oauth and the scope will be email
+        // Because those options were provided through bootstrap to DI, and we're overriding the provider.
+        let credentials = {
+            email: 'b@mail.com',
+            password: 'password'
+        }
+
+        this.auth.login(credentials, {
+            provider: AuthProviders.Password,
+            method: AuthMethods.Password
+        }).then((value) => {
+            console.log(value)
+        }).catch((error) => {
+            console.log(error)
+        });
     }
 }
