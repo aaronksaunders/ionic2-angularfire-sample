3c727de2cc30e8077d5e19ea21f315d90f97a7ad
Aaron Saunders <aaron@clearlyinnovative.com>
2017-01-22 15:55:54 -0500

    clean up

--- typings/main.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="main/ambient/es6-shim/index.d.ts" />
-/// <reference path="../node_modules/angularfire2/firebase3.d.ts" />
-/// <reference path="main/definitions/moment/index.d.ts" />
